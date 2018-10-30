a8a5e86812d7a046f92da43744c5e6e96fa06ee2
Ben Lee <1019229581@qq.com>
2018-10-30 11:13:09 +0800

    发现了动态页面的内容，只能等selenium出马了

--- test/testBs.py
+++ test/testBs.py
@@ -1,11 +1,18 @@
+#coding=utf-8
+# dynamic pages
+
+
 import urllib2
 from bs4 import BeautifulSoup
 
+span = '<span class="vote-count">9159</span>'
 url = 'http://baike.baidu.com/item/Python'
-soup = BeautifulSoup(urllib2.urlopen(url).read(), 'html.parser')
+# soup = BeautifulSoup(urllib2.urlopen(url).read(), 'html.parser')
+soup = BeautifulSoup(span, 'html.parser')
+print type(span)
 
 vote_tip = soup.find('span',"vote-tip")
 vote_node = soup.find('span',"vote-count")
 print type(vote_node)
 print vote_node.get_text().encode('utf-8')
-print vote_tip.get_text().encode('utf-8')
+# print vote_tip.get_text().encode('utf-8')
